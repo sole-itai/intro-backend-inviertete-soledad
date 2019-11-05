f8819d90bed4d39d2add12bdcc419d7684b05112
soledad itai gomez julian <sol_19somi@hotmail.com>
2019-11-04 22:36:09 -0600

    modificando app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express=require('express')
 const bodyParser =require('body-parser')
+const Product = require('./models/product')
+
 const app=express()
 
 app.use((req, res, next)=>{
-    res.header('Acces-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', '*')
 
-    res.header('Acces-Control-Allow-Headers', 'Origin, X-Requested- with, Content-Type, Acept, x-auth-token')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Acept, x-auth-token')
 
     res.header('Access-Control-Expose-Headers', 'x-auth-token')
     next()
@@ -25,6 +27,33 @@ app.post('/nombre', (req, res)=>{
     var name = req.body.name
     res.send ('Hola ' + name + 'un gusto conocerte')
 }) 
+
+app.post('/NewProduct', (req, res)=>{
+    var product = new Product
+
+    product.name = req.body.name
+    product.price = req.body.price
+    product.description = req.body.description
+    product.category = req.body.category
+    
+    product.save((err,productStored)=>{
+        if(err){
+        res.send(`Ocurrio un error guardando el producto: ${err}`)
+        }
+        res.send({product: productStored})
+    })
+})
 app.get('/backend')
+app.get ('/MyProducts', (req, res)=>{
+    Product.find({},(err,products)=>{
+        if (err){
+            res.send(`ocurrio un error: ${err}`)
+        }
+        if (!products){
+            res.send('Ocurrio un error')
+        }
+        res.send({products})
+    })
+})
 
 module.exports = app
